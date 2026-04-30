37f379e70adf2b2206d1fe43d2263df760b2523b
giusscos <giusscos@icloud.com>
2026-04-30 17:22:38 +0200

    Initial Commit

---
+++ DesertOasis/DesertOasisApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesertOasisApp.swift
+//  DesertOasis
+//
+//  Created by Giuseppe Cosenza on 30/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct DesertOasisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
